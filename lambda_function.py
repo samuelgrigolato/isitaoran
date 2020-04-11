2d68e38a55e2bfa964983253e00ee51fab233ae9
Samuel Grigolato <samuel.grigolato@gmail.com>
2020-04-11 19:43:19 -0300

    Add CORS headers on lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -8,13 +8,24 @@ def lambda_handler(event, context):
   if 'word' not in event['queryStringParameters']:
     return { 'statusCode': 400, 'body': '\'word\' parameter is required.' }
   word = event['queryStringParameters']['word']
-  article = aoran.resolve_article_for(word, database_file='/tmp/cmudict')
-  return {
-    'statusCode': 200,
-    'headers': {
-      'Content-Type': 'application/json'
-    },
-    'body': json.dumps({
-      'article': article
-    })
-  }
+  try:
+    article = aoran.resolve_article_for(word, database_file='/tmp/cmudict')
+    return {
+      'statusCode': 200,
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      'body': json.dumps({
+        'article': article
+      })
+    }
+  except aoran.WordNotFound:
+    return {
+      'statusCode': 404,
+      'headers': {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+
